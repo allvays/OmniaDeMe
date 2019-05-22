85d910fbd1af4401b4f3abe73cefed8226f43c41
Alyaev.R <alyaev@pkcc.ru>
2019-05-22 20:37:44 +0300

    Find views
    Find every view on EditPersonFragment

--- app/src/main/java/com/example/omniademe/fragments/PersonEditorFragment.java
+++ app/src/main/java/com/example/omniademe/fragments/PersonEditorFragment.java
@@ -9,6 +9,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.example.omniademe.R;
 
@@ -30,6 +34,14 @@ public class PersonEditorFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private EditText mNameEditText;
+    private EditText mBirthdayEditText;
+    private EditText mHeightEditText;
+    private RadioGroup mSexRadiogroup;
+    private RadioButton mMaleRB;
+    private RadioButton mFemaleRB;
+    private Button mSaveButton;
+
 
     private OnFragmentInteractionListener mListener;
 
@@ -67,16 +79,27 @@ public class PersonEditorFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_person_editor, container, false);
+        View v = inflater.inflate(R.layout.fragment_person_editor, container, false);
+
+        mNameEditText = v.findViewById(R.id.nameEditText);
+        mBirthdayEditText = v.findViewById(R.id.birthdayEditText);
+        mHeightEditText = v.findViewById(R.id.heightEditText);
+        mSexRadiogroup = v.findViewById(R.id.sexRadioGroup);
+        mMaleRB = v.findViewById(R.id.maleRB);
+        mFemaleRB = v.findViewById(R.id.femaleRB);
+        mSaveButton = v.findViewById(R.id.saveChangesButton);
+
+        return v;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+    /**
+     * // TODO: Rename method, update argument and hook method into UI event
+     * public void onButtonPressed(Uri uri) {
+     * if (mListener != null) {
+     * mListener.onFragmentInteraction(uri);
+     * }
+     * }
+     */
 
     @Override
     public void onAttach(Context context) {
